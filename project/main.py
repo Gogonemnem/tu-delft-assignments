17545785e9695bec76742d85231019b4bc832fe8
Gonem <gonemlau@gmail.com>
2021-09-23 10:10:31 +0200

    Fix errors & warnings

--- project/main.py
+++ project/main.py
@@ -1,31 +1,32 @@
-import PyQt5
-from PyQt5 import QtGui
-from PyQt5 import QtCore
-import traceback
+# import traceback
+# import PyQt5
+# from PyQt5 import QtGui
+# from PyQt5 import QtCore
 
-from PyQt5 import QtWidgets
+
 import sys
+from PyQt5 import QtWidgets
 
-class mainwindow(QtWidgets.QMainWindow):
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
-        super(mainwindow, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.setGeometry(400, 400, 400, 400)
         self.setWindowTitle("Breaksum")
         self.show()
 
     def catch_exceptions(t, val, tb):
-
         QtWidgets.QMessageBox.critical(None,
-                             "fout met Waarde",
-                             "Een ingevulde waarde veroorzaakt een fout in de berekening\n"
-                              f"type fout: {t}\n"
-                             f"traceback: {format_tb(tb)[-1]}")
+                                       "fout met Waarde",
+                                       "Een ingevulde waarde veroorzaakt een fout in de berekening\n"
+                                       f"type fout: {t}\n"
+                                       f"traceback: {format_tb(tb)[-1]}")
 
 
 hook = sys.excepthook
-sys.excepthook = mainwindow.catch_exceptions
+sys.excepthook = MainWindow.catch_exceptions
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = mainwindow()
+    window = MainWindow()
     app.exec_()
