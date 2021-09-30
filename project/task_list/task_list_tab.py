d0cb0a449fc1b04c19291e9713b336c0005ba4cb
Janine <j.d.timmerman@student.tudelft.nl>
2021-09-30 11:56:45 +0200

    External database as a txt file is added. (task_list_file in the project map) The TaskList class gets its data from that file and can delete or add new data to it. So if you now use the delete button, the txt file will be edited automatically after you close the application window, but the table on your screen will update immediately. You can only use the TaskList class when you call everything with main.py. Otherwise, you get an error. (perhaps I will fix that, but it can't find the file at the moment if you call it from somewhere else.)

--- project/task_list/task_list_tab.py
+++ project/task_list/task_list_tab.py
@@ -12,23 +12,8 @@ class TaskListTab(QtWidgets.QTableView):
         self.model = None
         self.delete_button = None
         self.edit_button = None  # those three self.statements aren't needed but pycharm is annoying
-
         self.database = TaskList()
         self.refresh()
-        # self.model = TaskListDatabase(self.database.data)
-        # self.setModel(self.model)
-        # self.resizeColumnsToContents()
-        # self.horizontalHeader().setMinimumSectionSize(250)
-        # self.horizontalHeader().setStretchLastSection(True)
-        #
-        # for index in range(len(self.database.data.index)):
-        #     self.delete_button = QtWidgets.QPushButton('Delete')
-        #     self.setIndexWidget(self.model.index(index, 5), self.delete_button)
-        #     self.delete_button.clicked.connect(self.delete_button_clicked)
-        #
-        #     self.edit_button = QtWidgets.QPushButton('Edit')
-        #     self.setIndexWidget(self.model.index(index, 6), self.edit_button)
-        #     self.edit_button.clicked.connect(self.edit_button_clicked)
 
     def delete_button_clicked(self):
         button = self.sender()
@@ -38,8 +23,6 @@ class TaskListTab(QtWidgets.QTableView):
             print(f'Row {index.row()} is deleted')
             self.database.delete_task(index.row())
             self.refresh()
-
-        # print(self.database.data)
 
     def edit_button_clicked(self):
         button = self.sender()
@@ -60,7 +43,6 @@ class TaskListTab(QtWidgets.QTableView):
             # 1. Give a pop-up where you can write data (reuse widget?), from where the original dataframe is edited
             # 2. use the edit possibility from TaskList(), but you can't control the input/ don't know how to store it?
             # 1 is probably easiest.
-
 
     def refresh(self):
         self.model = TaskListDatabase(self.database.data)
