ff8d1a674f930b5e9ee54eba70a8c8c8118b90c5
Gonem <gonemlau@gmail.com>
2021-10-02 14:31:01 +0200

    Add comments to agenda_widget.py

--- project/agenda/agenda_widget.py
+++ project/agenda/agenda_widget.py
@@ -27,22 +27,32 @@ class Widget(QtWidgets.QWidget):
         vlayout.addWidget(self.button, alignment=QtCore.Qt.AlignHCenter)
         vlayout.addWidget(self.browser)
 
+        # HOW DO WE PASS THE AGENDA OBJECT TO THE BUTTON?
+        # GLOBAL VARIABLE?
         self.button.clicked.connect(lambda: self.show_graph(agenda))
         self.resize(1000, 800)
 
     def show_graph(self, agenda):
         dics = []
-        today = agenda.agenda
-        for activity in today:
+        activities = agenda.agenda
+        for activity in activities:
+            # Turn every activity object attributes into a dictionary
+            # and add it to a list and turn it into a dataframe
             ac_dic = activity.__dict__
             dics.append({key: ac_dic[key] for key in ['activity', 'start_time', 'end_time', 'id']})
         df = pd.DataFrame(dics)
-        if today:
-            x_start = today[0].start_time
+
+        # Determine range of the x-axis
+        if activities:
+            x_start = activities[0].start_time
             x_range = [x_start, x_start+timedelta(days=1)]
         else:
             x_range = None
+
+        # Plot the figure
         fig = px.timeline(
             df, x_start="start_time", x_end="end_time", y="activity", color="id", range_x=x_range)
         fig.update_yaxes(autorange="reversed")  # otherwise tasks are listed from the bottom up
+
+        # Turn the HTML plot into a widget and do not open it within a browser
         self.browser.setHtml(fig.to_html(include_plotlyjs='cdn'))
