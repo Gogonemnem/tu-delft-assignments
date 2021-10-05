206384016dbc941dd5c17602c6cb89f714e8e3dc
Janine <j.d.timmerman@student.tudelft.nl>
2021-10-05 10:34:57 +0200

    -the class TaskList now used pd.read_csv and pd.to_csv to read and write the external database -class TaskObject is added, which makes an object from a task -def data_output is updated to give a list of taskobjects

--- project/task_list/data_for_database.py
+++ project/task_list/data_for_database.py
@@ -4,49 +4,39 @@ import pandas as pd
 class TaskList:
 
     def __init__(self):
-        self.data = pd.DataFrame({'Task': [],
-                                  'Estimated time (minutes)': [],
-                                  'Priority': [],
-                                  'Periodic': [],
-                                  'Preferred time': [],
-                                  'Delete task': [],
-                                  'Edit task': []})
-        self.add_file_data()
+        self.data = self.add_file_data()
 
-    def __add_task(self, task):
+    def add_task(self, task):
         new_task = {'Task': task[0], 'Estimated time (minutes)': task[1], 'Priority': task[2],
                     'Periodic': task[3], 'Preferred time': task[4], 'Delete task': None, 'Edit task': None}
         self.data = self.data.append(new_task, ignore_index=True)
+        self.__write_to_file()
 
     def delete_task(self, index):
         self.data = self.data.drop(index)
         self.data = self.data.reset_index(drop=True)
+        self.__write_to_file()
 
-        with open('task_list_file', 'r') as file:
-            lines = file.readlines()
-        with open('task_list_file', 'w') as file:
-            count = 0
-            for line in lines:
-                if count != index:
-                    file.write(line)
-                count += 1
+    @staticmethod
+    def add_file_data():
+        return pd.read_csv('task_list_file', sep='$')
 
-    def add_file_data(self):
-        with open('task_list_file', 'r') as file:
-            for line in file:
-                new_line = line.strip().split(';')
-                self.__add_task(new_line)
-
-    def write_to_file(self, task):
-        with open('task_list_file', 'a') as file:
-            print(*task, sep=';', file=file)
-
-        self.__add_task(task)
+    def __write_to_file(self):
+        self.data.to_csv('task_list_file', sep='$', index=False)
 
     def data_output(self):
-        data_dict = dict()
+        task_object_list = []
         for i in range(len(self.data)):
-            data_dict[self.data.iloc[i][0]] = list(self.data.iloc[i][1:5])
+            task_object_list.append(TaskObject(*self.data.iloc[i][:5]))
 
-        return data_dict
+        return task_object_list
 
+
+class TaskObject:
+
+    def __init__(self, name, time, priority, periodic, preferred_time):
+        self.name = name
+        self.time = time
+        self.priority = priority
+        self.periodic = periodic
+        self.preferred_time = preferred_time
