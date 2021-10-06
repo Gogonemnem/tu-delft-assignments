7726a0a077a8a6f82db8b75c6d3b91734f2bfe89
Gonem Lau <gonemlau@gmail.com>
2021-10-06 15:14:03 +0200

    Add test for modifying Activity

--- project/test/test_agenda.py
+++ project/test/test_agenda.py
@@ -27,6 +27,32 @@ class TestAgenda(unittest.TestCase):
             self.assertEqual(activity.duration, duration)
             self.assertEqual(activity.summary, summary)
 
+    def test_modify_activity_attributes(self):
+        activity_name = 'Work'
+        now = datetime.now()
+        duration = timedelta(hours=1)
+        end_time = now + duration
+        summary = 'Meeting with Alice & Bob, but not with Eve'
+        activities = [
+            Activity(activity_name, now, duration=duration, summary=summary),
+            Activity(activity_name, now, end_time=end_time, summary=summary),
+            Activity(activity_name, now, duration=duration, end_time=end_time, summary=summary)
+        ]
+
+        activities[0].modify_activity(activity='No Work', start_time=now+duration, duration=duration)
+        self.assertEqual(activities[0].activity, 'No Work')
+        self.assertEqual(activities[0].start_time, now+duration)
+        self.assertEqual(activities[0].end_time, end_time+duration)
+        self.assertEqual(activities[0].duration, duration)
+        self.assertEqual(activities[0].summary, summary)
+
+        for activity in activities[1:]:
+            self.assertEqual(activity.activity, activity_name)
+            self.assertEqual(activity.start_time, now)
+            self.assertEqual(activity.end_time, end_time)
+            self.assertEqual(activity.duration, duration)
+            self.assertEqual(activity.summary, summary)
+
     def test_invalid_activity(self):
         activity_name = 'Work'
         now = datetime.now()
