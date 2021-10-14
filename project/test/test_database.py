2481bba32a01cc50d72341369df171fcd3eb6136
Janine <j.d.timmerman@student.tudelft.nl>
2021-10-14 11:33:21 +0200

    New test for the database

--- project/test/test_database.py
+++ project/test/test_database.py
@@ -43,4 +43,46 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(object_list[1].time, 10)
         self.assertEqual(object_list[1].priority, 'low')
         self.assertTrue(object_list[0].periodic)
+        self.assertEqual(object_list[0].preferred_time, 'Whole day')
         self.assertEqual(len(object_list), len(task_list.data))
+
+    def test_task_object(self):
+        task_object = TaskObject('Water the plants', 5, 'high', False, 'Whole day')
+        attributes = ['name', 'time', 'priority', 'periodic', 'preferred_time']
+
+        for attribute in attributes:
+            self.assertTrue(hasattr(task_object, attribute))
+
+    def test_edit_task(self):
+        task_list = TaskList()
+        categories = ['Task', 'Estimated time (minutes)', 'Priority', 'Periodic', 'Preferred time']
+        original_values = ['Take a walk', 30, 'normal', True, 'Whole day']
+        new_values = ['Take a very long walk', 5, 'high', False, 'Afternoon']
+
+        # test the original values
+        for i in range(len(original_values)):
+            self.assertEqual(task_list.data.iloc[0][i], original_values[i])
+
+        # edit the task
+        for i in range(len(categories)):
+            task_list.edit_task(0, categories[i], new_values[i])
+
+        # test if the tasks are changed
+        for i in range(len(categories)):
+            self.assertEqual(task_list.data.iloc[0][i], new_values[i])
+
+        # change it back
+        for i in range(len(categories)):
+            task_list.edit_task(0, categories[i], original_values[i])
+
+        # test if the change back worked
+        for i in range(len(original_values)):
+            self.assertEqual(task_list.data.iloc[0][i], original_values[i])
+
+    def test_create_dataframe(self):
+        database = TaskList.add_file_data()
+        task_list = TaskList()
+
+        self.assertIsInstance(database, pd.DataFrame)
+        self.assertIsInstance(task_list.data, pd.DataFrame)
+        self.assertTrue(hasattr(task_list, 'data'))
