6f25139e85888642cc2152cf0a0379b9671b794e
Gonem Lau <g.lau@student.tudelft.nl>
2021-10-14 11:11:33 +0200

    add auto test

--- project/test/test_time.py
+++ project/test/test_time.py
@@ -1,10 +1,13 @@
 import unittest
 import sys
 
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QWidget
 
 from project.agenda.agenda import Agenda
 from project.randomizer.optimal_time import TimeRandomizer
+import pytestqt
+# import qtbot
 
 
 # class TestTime1(unittest.TestCase):
@@ -23,26 +26,40 @@ from project.randomizer.optimal_time import TimeRandomizer
 
 
 class TestTime2(unittest.TestCase):
-
-    @staticmethod
-    def main(lst):
+    def main(self, lst):
         app = QApplication(sys.argv)
         wdgt = QWidget()
         agenda0 = Agenda()
         time_randomizer = TimeRandomizer(lst, agenda0)
         time_randomizer.start()
 
+        QTimer.singleShot(6 * 1000, self.close)
+
         wdgt.show()
         app.exec_()
+
+    def close(self):
+        QApplication.quit()
 
     def test_do_all_tasks(self):
         lst = [1, 2, 3, 4]
         self.main(lst)
         print(lst)
-        self.assertEqual(len(lst), 0)
+        self.assertEqual(0, len(lst))
 
     def test_do_all_tasks1(self):
         lst = [1, 2, 3, 4]
         self.main(lst)
         print(lst)
-        self.assertEqual(len(lst), 0)
+        self.assertEqual(0, len(lst))
+
+
+# @qtbot.fixture
+# def test_hello(qtbot):
+#     widget = HelloWidget()
+#     qtbot.addWidget(widget)
+#
+#     # click in the Greet button and make sure it updates the appropriate label
+#     qtbot.mouseClick(widget.button_greet, qt_api.QtCore.Qt.MouseButton.LeftButton)
+#
+#     assert widget.greet_label.text() == "Hello!"
