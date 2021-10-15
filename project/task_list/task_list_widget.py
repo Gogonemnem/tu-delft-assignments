756b228311584d7eb08b2692c68b4c7b93904b25
Nils <n.molenaar@student.tudelft.nl>
2021-10-15 10:59:58 +0200

    "To do list" and "create to do list" in seperate class so it can be called without calling the to do list widget

--- project/task_list/task_list_widget.py
+++ project/task_list/task_list_widget.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWidgets import QPushButton, QRadioButton, QGridLayout, QButtonGroup, QGroupBox
-from project.randomizer.randomizer_of_tasks import Randomizer
-from project.task_list.to_do_list import ToDoList
+from project.task_list.to_do_list import ToDoList, CreateToDoList
 
 
 class TaskListWidget(QGroupBox):
@@ -14,7 +13,7 @@ class TaskListWidget(QGroupBox):
         self.group_remove = QButtonGroup()
         self.group_done = QButtonGroup()
         self.group_doing = QButtonGroup()
-        self.tasks = self.initial()
+        self.tasks = CreateToDoList().list()
 
         for item in self.tasks:
             self.task = QRadioButton(f'Button {i + 1}', self)
@@ -44,31 +43,15 @@ class TaskListWidget(QGroupBox):
             self.setLayout(self.layout)
             i += 1
 
-        self.NumberOfButtons = len(self.group_task.buttons())
+        self.length_list = len(self.group_task.buttons())
         self.group_task.buttonClicked.connect(self.select)
         self.group_remove.buttonClicked.connect(self.removed)
         self.group_done.buttonClicked.connect(self.completed)
         self.group_doing.buttonClicked.connect(self.ongoing)
         self.select()
 
-    @staticmethod
-    def initial():
-        tasks_list = []
-        tasks_list.extend(Randomizer().randomize_tasks_other_morning())
-        tasks_list.extend(Randomizer().randomize_tasks_other_afternoon())
-        tasks_list.extend(Randomizer().randomize_tasks_other_evening())
-        file = open("ToDoList.txt", 'w')
-        for i in range(len(tasks_list)):
-            file.writelines(tasks_list[i] + '\n')
-        file.close()
-        file2 = open("DoingList.txt", 'w')
-        file2.close()
-        file3 = open("DoneList.txt", 'w')
-        file3.close()
-        return tasks_list
-
     def select(self):
-        for i in range(self.NumberOfButtons):
+        for i in range(self.length_list):
             if not self.group_task.button(i) is None and\
                     i == self.group_task.id(self.group_task.checkedButton()):
                 # if i == self.button_group_task.id(self.button_group_task.checkedButton()):
@@ -80,7 +63,7 @@ class TaskListWidget(QGroupBox):
                 selected_done.setStyleSheet("background-color:  rgb(100, 175, 100)")
                 selected_doing = self.group_doing.button(i)
                 selected_doing.setDisabled(False)
-                selected_doing.setStyleSheet("background-color:  rgb(20, 100, 125)")
+                selected_doing.setStyleSheet("background-color:  rgb(40, 125, 175)")
 
             elif not self.group_task.button(i) is None:
                 selected_remove = self.group_remove.button(i)
@@ -94,7 +77,7 @@ class TaskListWidget(QGroupBox):
                 selected_doing.setStyleSheet("background-color:  rgb(50, 200, 255)")
 
     def removed(self):
-        for i in range(self.NumberOfButtons):
+        for i in range(self.length_list):
             if i == self.group_task.id(self.group_task.checkedButton()):
                 ToDoList.remove(self.group_task.checkedButton().text().replace(
                     f'Task {i + 1} for today is: ', ''))
@@ -104,7 +87,7 @@ class TaskListWidget(QGroupBox):
                 self.group_doing.button(i).setVisible(False)
 
     def ongoing(self):
-        for i in range(self.NumberOfButtons):
+        for i in range(self.length_list):
             if i == self.group_doing.id(self.group_doing.checkedButton()):
                 ToDoList.execute(self.group_task.checkedButton().text().replace(
                     f'Task {i + 1} for today is: ', ''))
@@ -112,11 +95,10 @@ class TaskListWidget(QGroupBox):
                 self.group_done.button(i).setVisible(True)
 
     def completed(self):
-        for i in range(self.NumberOfButtons):
+        for i in range(self.length_list):
             if i == self.group_task.id(self.group_task.checkedButton()):
                 ToDoList.complete(self.group_task.checkedButton().text().replace(
                     f'Task {i + 1} for today is: ', ''))
-                print(ToDoList().done())
                 selected_task = self.group_task.button(i)
                 selected_task.setStyleSheet("color:  rgb(100, 175, 100)")
                 selected_task.setText('\u2713' + 'Completed:' + selected_task.text().replace(
@@ -132,9 +114,9 @@ class TaskListWidget(QGroupBox):
                 self.group_task.removeButton(self.group_task.button(i))
                 self.group_done.removeButton(self.group_done.button(i))
                 self.group_doing.removeButton(self.group_doing.button(i))
-                self.layout.addWidget(selected_task, self.NumberOfButtons + self.complete, 0)
-                self.layout.addWidget(selected_doing, self.NumberOfButtons + self.complete, 1)
-                self.layout.addWidget(selected_remove, self.NumberOfButtons + self.complete, 2)
-                self.layout.addWidget(selected_done, self.NumberOfButtons + self.complete, 2)
+                self.layout.addWidget(selected_task, self.length_list + self.complete, 0)
+                self.layout.addWidget(selected_doing, self.length_list + self.complete, 1)
+                self.layout.addWidget(selected_remove, self.length_list + self.complete, 2)
+                self.layout.addWidget(selected_done, self.length_list + self.complete, 2)
                 self.setLayout(self.layout)
                 self.complete += 1
--- project/task_list/to_do_list.py
+++ project/task_list/to_do_list.py
@@ -1,3 +1,5 @@
+from project.randomizer.randomizer_of_tasks import Randomizer
+
 
 class ToDoList:
     def __init__(self):
@@ -7,58 +9,51 @@ class ToDoList:
         self.status()
 
     def status(self):
-        file = open("ToDoList.txt", 'r')
-        for lines in file:
-            self.task_todo.append(lines)
-        file.close()
-        file2 = open("DoingList.txt", 'r')
-        for lines in file2:
-            self.task_doing.append(lines)
-        file2.close()
-        file3 = open("DoneList.txt", 'r')
-        for lines in file3:
-            self.task_done.append(lines)
-        file3.close()
+        with open("ToDoList.txt", 'r', encoding='utf-7') as file_to_do:
+            for lines in file_to_do:
+                self.task_todo.append(lines)
+        with open("DoingList.txt", 'r', encoding='utf-7') as file_doing:
+            for lines in file_doing:
+                self.task_doing.append(lines)
+        with open("DoneList.txt", 'r', encoding='utf-7') as file_done:
+            for lines in file_done:
+                self.task_done.append(lines)
 
     @staticmethod
     def execute(task):
-        file = open("DoingList.txt", 'a')
-        file.writelines(task)
-        file.close()
-        file2 = open("ToDoList.txt", 'r')
-        lines = file2.readlines()
-        file2.close()
-        file2 = open("ToDoList.txt", 'w')
-        for line in lines:
-            if not task == line:
-                file2.writelines(line)
-        file2.close()
+        with open("DoingList.txt", 'a', encoding='utf-7') as file_doing:
+            file_doing.writelines(task)
+            file_doing.truncate()
+
+        with open("ToDoList.txt", "r+", encoding='utf-7') as file_to_do:
+            lines = file_to_do.readlines()
+            file_to_do.seek(0)
+            for i in lines:
+                if not i.replace("\n", "") == task:
+                    file_to_do.write(i)
+            file_to_do.truncate()
 
     @staticmethod
     def complete(task):
-        file = open("DoneList.txt", 'a')
-        file.writelines(task)
-        file.close()
-        file2 = open("Doinglist.txt", 'r')
-        lines = file2.readlines()
-        file2.close()
-        file2 = open("Doinglist.txt", 'w')
-        for line in lines:
-            if not task == line:
-                file2.writelines(line)
-        file2.close()
+        with open("DoneList.txt", 'a', encoding='utf-7') as file_done:
+            file_done.writelines(task)
+        with open("Doinglist.txt", "r+", encoding='utf-7') as file_doing:
+            lines = file_doing.readlines()
+            file_doing.seek(0)
+            for i in lines:
+                if not i.replace("\n", "") == task:
+                    file_doing.write(i)
+            file_doing.truncate()
 
     @staticmethod
     def remove(task):
-        file = open("ToDoList.txt", 'r')
-        lines = file.readlines()
-        file.close()
-        file = open("ToDoList.txt", 'w')
-
-        for line in lines:
-            if not task == line.replace("\n", ""):
-                file.writelines(line)
-        file.close()
+        with open("Doinglist.txt", "r+", encoding='utf-7') as file_to_do:
+            lines = file_to_do.readlines()
+            file_to_do.seek(0)
+            for i in lines:
+                if not i == task:
+                    file_to_do.write(i)
+            file_to_do.truncate()
 
     def todo(self):
         return self.task_todo
@@ -68,3 +63,23 @@ class ToDoList:
 
     def done(self):
         return self.task_done
+
+
+class CreateToDoList:
+    def __init__(self):
+        self.todo_list = self.list()
+
+    @staticmethod
+    def list():
+        tasks_list = []
+        tasks_list.extend(Randomizer().randomize_tasks_other_morning())
+        tasks_list.extend(Randomizer().randomize_tasks_other_afternoon())
+        tasks_list.extend(Randomizer().randomize_tasks_other_evening())
+        with open("ToDoList.txt", 'w', encoding='utf-7') as file_todo:
+            for i in enumerate(tasks_list):
+                file_todo.writelines(tasks_list[i] + '\n')
+        with open("DoingList.txt", 'w', encoding='utf-7') as file_doing:
+            file_doing.close()
+        with open("DoneList.txt", 'w', encoding='utf-7') as file_done:
+            file_done.close()
+        return tasks_list
