b1b02cab2b42483f092892685bb6c3be1331a6d9
Gonem Lau <g.lau@student.tudelft.nl>
2021-10-16 13:59:41 +0200

    Make activity a dataclass, fix modify activity so order is correctly restored

--- project/test/test_agenda.py
+++ project/test/test_agenda.py
@@ -15,9 +15,8 @@ class TestAgenda(unittest.TestCase):
         end_time = now + duration
         summary = 'Meeting with Alice & Bob, but not with Eve'
         activities = [
-            Activity(activity_name, now, duration=duration, summary=summary),
-            Activity(activity_name, now, end_time=end_time, summary=summary),
-            Activity(activity_name, now, duration=duration, end_time=end_time, summary=summary)
+            Activity(activity_name, now, duration, summary=summary),
+            Activity(activity_name, now, end_time, summary=summary),
         ]
 
         for activity in activities:
@@ -27,72 +26,50 @@ class TestAgenda(unittest.TestCase):
             self.assertEqual(activity.duration, duration)
             self.assertEqual(activity.summary, summary)
 
+    def test_invalid_activity(self):
+        activity_name = 'Work'
+        now = datetime.now()
+
+        with self.assertRaises(TypeError):
+            Activity(activity_name, now, None)
+
+        with self.assertRaises(TypeError):
+            Activity(activity_name, now, '17:31')
+
     def test_modify_activity_attributes(self):
         activity_name = 'Work'
         now = datetime.now()
         duration = timedelta(hours=1)
         end_time = now + duration
         summary = 'Meeting with Alice & Bob, but not with Eve'
-        activities = [
-            Activity(activity_name, now, duration=duration, summary=summary),
-            Activity(activity_name, now, end_time=end_time, summary=summary),
-            Activity(activity_name, now, duration=duration, end_time=end_time, summary=summary)
-        ]
 
-        activities[0].modify_activity(
-            activity='No Work', start_time=now+duration, duration=duration)
-        self.assertEqual(activities[0].activity, 'No Work')
-        self.assertEqual(activities[0].start_time, now+duration)
-        self.assertEqual(activities[0].end_time, end_time+duration)
-        self.assertEqual(activities[0].duration, duration)
-        self.assertEqual(activities[0].summary, summary)
+        activity_old = Activity(activity_name, end_time, duration, summary=summary)
+        activity_change = Activity(activity_name, now, duration, summary=summary)
+        activity_new = Activity('No Work', now + 2 * duration, duration, summary)
 
-        for activity in activities[1:]:
-            self.assertEqual(activity.activity, activity_name)
-            self.assertEqual(activity.start_time, now)
-            self.assertEqual(activity.end_time, end_time)
-            self.assertEqual(activity.duration, duration)
-            self.assertEqual(activity.summary, summary)
+        agenda = Agenda()
+        agenda.add_activity(activity_change)  # will get index 0
+        agenda.add_activity(activity_old)
 
-    def test_invalid_activity(self):
-        activity_name = 'Work'
-        now = datetime.now()
-        duration = timedelta(hours=1)
-        end_time = now + duration
-        # with self.assertRaises(TypeError):
-        #     Activity()
+        # change so that index will be 1
+        agenda.modify_activity(0, activity='No Work', start_time=now+2*duration, end_or_dur=duration)
 
-        with self.assertRaises(ValueError):
-            Activity(activity_name, now)
-
-        with self.assertRaises(ValueError):
-            Activity(activity_name, '17:31', duration=duration)
-
-        with self.assertRaises(ValueError):
-            Activity(activity_name, '17:31', end_time=end_time)
-
-        with self.assertRaises(ValueError):
-            Activity(activity_name, now, duration='17:31')
-
-        with self.assertRaises(ValueError):
-            Activity(activity_name, now, end_time='17:31')
-
-        with self.assertRaises(ValueError):
-            Activity(activity_name, now, duration=duration, end_time=end_time+duration)
+        self.assertEqual(agenda.agenda[1], activity_new)
+        self.assertEqual(agenda.agenda[0], activity_old)
 
     def test_old_activity_removed(self):
         agenda = Agenda()
         now = datetime.now()
         duration = timedelta(hours=1)
-        activity0 = Activity('Work', now - 1/2 * duration, duration=duration)
-        activity1 = Activity('Work', now, duration=duration)
+        activity0 = Activity('Work', now - 1/2 * duration, duration)
+        activity1 = Activity('Work', now, duration)
         activities = [
             # is needed to check whether elements of self.agenda are equal
             activity0,
             activity1,
             # these are gonna be deleted
-            Activity('Work', now-2*duration, duration=duration),
-            Activity('Work', now-3*duration, duration=duration)
+            Activity('Work', now-2*duration, duration),
+            Activity('Work', now-3*duration, duration)
         ]
 
         for activity in activities:
@@ -108,8 +85,8 @@ class TestAgenda(unittest.TestCase):
         duration = timedelta(hours=1)
 
         # today
-        activity0 = Activity('Work', now, duration=duration)
-        activity1 = Activity('Work', now - 1/2 * duration, duration=duration)
+        activity0 = Activity('Work', now, duration)
+        activity1 = Activity('Work', now - 1/2 * duration, duration)
 
         activities = [
             # is needed to check whether elements of self.agenda are equal
@@ -117,12 +94,12 @@ class TestAgenda(unittest.TestCase):
             activity1,
 
             # these are gonna be deleted
-            Activity('Work', now + day, duration=duration),
-            Activity('Work', now + 2 * day, duration=duration),
-            Activity('Work', now + 3 * day, duration=duration),
-            Activity('Work', now - day, duration=duration),
-            Activity('Work', now - 2 * day, duration=duration),
-            Activity('Work', now - 3 * day, duration=duration)
+            Activity('Work', now + day, duration),
+            Activity('Work', now + 2 * day, duration),
+            Activity('Work', now + 3 * day, duration),
+            Activity('Work', now - day, duration),
+            Activity('Work', now - 2 * day, duration),
+            Activity('Work', now - 3 * day, duration)
         ]
 
         for activity in activities:
