162911f09b7b280125c00e888f67771d4eee22cc
Gonem Lau <gonemlau@gmail.com>
2021-10-20 04:20:47 +0200

    Refactor/Rework to_do_list.py, add some evening tasks so the main runs

--- project/task_list/task_list_widget.py
+++ project/task_list/task_list_widget.py
@@ -1,10 +1,16 @@
 from PyQt5.QtWidgets import QPushButton, QRadioButton, QGridLayout, QButtonGroup, QGroupBox
-from project.task_list.to_do_list import ToDoList, CreateToDoList
+
+from project.randomizer.optimal_time import TimeRandomizer
+from project.task_list.to_do_list import ToDoList
 
 
 class TaskListWidget(QGroupBox):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.todolist = ToDoList()
+        self.time_randomizer = TimeRandomizer(self.todolist.todolist)
+
         self.setTitle("Daily to-do list")
         self.complete = 0
 
@@ -12,140 +18,141 @@ class TaskListWidget(QGroupBox):
         self.group_remove = QButtonGroup()
         self.group_done = QButtonGroup()
         self.group_doing = QButtonGroup()
-        self.tasks = CreateToDoList.list(new=True)
+
+        # self.group_task = []
+        # self.group_remove = []
+        # self.group_done = []
+        # self.group_doing = []
 
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
-        for i, item in enumerate(self.tasks):
-            self.task = self.create_task_select(i, item)
-            self.remove = self.create_remove_button(i)
-            self.doing = self.create_doing_button(i)
-            self.done = self.create_done_button(i)
+        self.create_to_do_list_visual()
 
-            self.color_buttons(i)
+        self.generate_new = QPushButton('Generate new to do list', self)
+        self.layout.addWidget(self.generate_new, 99, 0, 1, 3)
+        self.generate_new.clicked.connect(self.refresh)
 
-        # self.length_list = len(self.group_task.buttons())
+    def create_to_do_list_visual(self):
+        self.todolist.status()
+        for task in self.todolist.todolist:
+            identifier = int(task['ID'])
 
-    def removed(self, index):
+            self.create_remove_button(identifier)
+            self.create_doing_button(identifier)
+            self.create_done_button(identifier)
+            self.create_task_select(identifier, task['Task'])
+
+            self.color_buttons(identifier)
+
+    def removed(self, identifier):
         """Remove task from to-do list."""
-        task_button = self.group_task.button(index)
-        if task_button.isChecked():
-            ToDoList.change(task_button.text().replace(
-                f'Task {index + 1} for today is: ', ''), self.group_task.id(self.group_task.checkedButton()), "Removed")
-            self.group_task.button(index).setVisible(False)
-            self.group_done.button(index).setVisible(False)
-            self.group_remove.button(index).setVisible(False)
-            self.group_doing.button(index).setVisible(False)
+        self.todolist.remove_task(identifier)
+        self.layout.removeWidget(self.group_task.button(identifier))
+        self.layout.removeWidget(self.group_done.button(identifier))
+        self.layout.removeWidget(self.group_remove.button(identifier))
+        self.layout.removeWidget(self.group_doing.button(identifier))
 
-    def ongoing(self, index):
+    def ongoing(self, identifier):
         """Set status of task to "Doing"."""
-        task_button = self.group_task.button(index)
-        if task_button.isChecked():
-            new_text = task_button.text().replace(f'Task {index + 1} for today is: ', '')
-            ToDoList.change(new_text, index, "Doing")
-            self.group_remove.button(index).setVisible(False)
-            self.group_done.button(index).setVisible(True)
+        task_button = self.group_task.button(identifier)
 
-    def completed(self, index):
+        task = next((item for item in self.todolist.todolist if int(item["ID"]) == identifier), None)
+        task_button.setText(f'Doing task {identifier} for today: ' + task['Task'])
+
+        self.todolist.change(identifier, "Doing")
+        self.group_remove.button(identifier).setVisible(False)
+        self.group_done.button(identifier).setVisible(True)
+
+    def completed(self, identifier):
         """Set status of task to "Done"."""
-        task_button = self.group_task.button(index)
-        if task_button.isChecked():
-            old_text = f'Task {index + 1} for today is: '
-            ToDoList.change(task_button.text().replace(old_text, ''), index, "Done")
+        task_button = self.group_task.button(identifier)
+        task = next((item for item in self.todolist.todolist if int(item["ID"]) == identifier), None)
+        task_button.setText('\u2713' + 'Completed: ' + task['Task'])
+        self.todolist.change(identifier, "Done")
 
-            selected_task = self.group_task.button(index)
-            selected_doing = self.group_doing.button(index)
-            selected_done = self.group_done.button(index)
-            # selected_remove = self.group_remove.button(index)
+        selected_task = self.group_task.button(identifier)
+        selected_doing = self.group_doing.button(identifier)
+        selected_done = self.group_done.button(identifier)
+        selected_remove = self.group_remove.button(identifier)
 
-            selected_task.setDisabled(True)
-            selected_doing.setVisible(False)
-            selected_done.setVisible(False)
+        selected_doing.setVisible(False)
+        selected_done.setVisible(False)
+        selected_remove.setVisible(True)
 
-            # ## Do we want this?????
-            # ## If so, selected_task may not be disabled!
-            # selected_remove.setVisible(True)
+        selected_task.setStyleSheet("color:  rgb(100, 175, 100)")
+        self.complete += 1
 
-            selected_task.setStyleSheet("color:  rgb(100, 175, 100)")
-            selected_task.setText(
-                selected_task.text().replace(old_text, '\u2713' + 'Completed: '))
-            # selected_done.setStyleSheet("background-color:  rgb(175, 175, 175)")
-
-            # ## Personal taste Question: Do we want to reorder the tasks?
-            # ## I actually like it staying in order like this
-            # ## This will also make our code a bit shorter
-            #
-            # self.group_task.removeButton(selected_task)
-            # self.group_doing.removeButton(selected_doing)
-            # self.group_done.removeButton(selected_done)
-            # self.group_remove.removeButton(selected_remove)
-            #
-            # self.layout.addWidget(selected_task, self.length_list + self.complete, 0)
-            # self.layout.addWidget(selected_doing, self.length_list + self.complete, 1)
-            # self.layout.addWidget(selected_done, self.length_list + self.complete, 2)
-            self.complete += 1
-
-    def create_task_select(self, index, item):
+    def create_task_select(self, identifier, item):
         """Visualize the selection radio button"""
-        task = QRadioButton(f'Task {index + 1} for today is: {item}')
+        index = next((i for i, item in enumerate(self.todolist.todolist) if int(item["ID"]) == identifier), None)
+
+        text = f'Task {identifier} for today is: {item}'
+        task = QRadioButton(text, self)
         task.setMinimumWidth(450)
 
-        task.toggled.connect(lambda: self.color_buttons(index))
+        task.toggled.connect(lambda: self.color_buttons(identifier))
 
-        self.group_task.addButton(task, index)
-        self.layout.addWidget(task, index, 0)
+        self.group_task.addButton(task, identifier)
+        self.layout.addWidget(task, identifier, 0)
+
+        task = self.todolist.todolist[index]
+
+        if task['Task Status'] == 'Done':
+            self.completed(identifier)
+        elif task['Task Status'] == 'Doing':
+            self.ongoing(identifier)
 
         return task
 
-    def create_remove_button(self, index):
+    def create_remove_button(self, identifier):
         """Visualize the selection remove button"""
-        remove = QPushButton('Remove task')
+        remove = QPushButton('Remove task', self)
         remove.setCheckable(True)
         remove.setMaximumWidth(100)
 
-        remove.clicked.connect(lambda: self.removed(index))
+        remove.clicked.connect(lambda: self.removed(identifier))
 
-        self.group_remove.addButton(remove, index)
-        self.layout.addWidget(remove, index, 2)
+        self.group_remove.addButton(remove, identifier)
+        self.layout.addWidget(remove, identifier, 2)
 
         return remove
 
-    def create_doing_button(self, index):
+    def create_doing_button(self, identifier):
         """Visualize the selection doing button"""
-        doing = QPushButton('Do task')
+        doing = QPushButton('Do task', self)
         doing.setCheckable(True)
         doing.setMaximumWidth(100)
 
-        doing.clicked.connect(lambda: self.ongoing(index))
+        doing.clicked.connect(lambda: self.ongoing(identifier))
 
-        self.group_doing.addButton(doing, index)
-        self.layout.addWidget(doing, index, 1)
+        self.group_doing.addButton(doing, identifier)
+        self.layout.addWidget(doing, identifier, 1)
 
         return doing
 
-    def create_done_button(self, index):
+    def create_done_button(self, identifier):
         """Visualize the selection done button"""
-        done = QPushButton('Task completed')
+        done = QPushButton('Task completed', self)
         done.setCheckable(True)
         done.setVisible(False)
         done.setMaximumWidth(100)
 
-        done.clicked.connect(lambda: self.completed(index))
+        done.clicked.connect(lambda: self.completed(identifier))
 
-        self.group_done.addButton(done, index)
-        self.layout.addWidget(done, index, 2)
+        self.group_done.addButton(done, identifier)
+        self.layout.addWidget(done, identifier, 2)
 
         return done
 
-    def color_buttons(self, index):
+    def color_buttons(self, identifier):
         """Color selected task and accompanying buttons."""
 
-        selected_remove = self.group_remove.button(index)
-        selected_done = self.group_done.button(index)
-        selected_doing = self.group_doing.button(index)
+        selected_remove = self.group_remove.button(identifier)
+        selected_done = self.group_done.button(identifier)
+        selected_doing = self.group_doing.button(identifier)
 
-        is_checked = self.group_task.button(index).isChecked()
+        is_checked = self.group_task.button(identifier).isChecked()
 
         selected_remove.setEnabled(is_checked)
         selected_done.setEnabled(is_checked)
@@ -160,3 +167,12 @@ class TaskListWidget(QGroupBox):
             selected_remove.setStyleSheet("background-color:  rgb(225, 175, 175)")
             selected_done.setStyleSheet("background-color:  rgb(200, 225, 200)")
             selected_doing.setStyleSheet("background-color:  rgb(50, 200, 255)")
+
+    def refresh(self):
+        self.clear_widget()
+        self.create_to_do_list_visual()
+
+    def clear_widget(self):
+        for item in self.group_task.buttons():
+            identifier = self.group_task.id(item)
+            self.removed(identifier)
--- project/task_list/to_do_list.py
+++ project/task_list/to_do_list.py
@@ -1,74 +1,74 @@
 import os
+import csv
 from project.randomizer.randomizer_of_tasks import Randomizer
-from pathlib import Path
-folder = Path(os.getcwd())
+
+absolute_path = os.path.abspath(__file__)
+fileDirectory = os.path.dirname(absolute_path)
+parent = os.path.dirname(fileDirectory)
+path = os.path.join(parent, 'main', 'todolist')
 
 
 class ToDoList:
     def __init__(self):
-        self.task_todo = []
-        self.task_doing = []
-        self.task_done = []
+        self.todolist: list[dict] = []
         self.status()
 
     def status(self):
         """Check status of all tasks in To-Do list file."""
+        self.read_file()
 
-        with open(folder / "ToDoList.txt", 'r', encoding='utf-7') as file_to_do:
-            for lines in file_to_do:
-                if lines.split("&")[1] == "To-Do" or lines.split("&")[1] == "Snooze" or\
-                        lines.split("&")[1] == "Reschedule":
-                    self.task_todo.append(lines.split("&")[0])
-                elif lines.split("&")[1] == "Doing":
-                    self.task_doing.append(lines.split("&")[0])
-                else:
-                    self.task_done.append(lines.split("&")[0])
+        not_completed = [task for task in self.todolist if task['Task Status'] != 'Done']
 
-    @staticmethod
-    def change(task, num, status):
-        """Change status of task [task] with id [num] from to-do list to status [status]."""
+        if not not_completed:
+            self.create_todolist()
 
-        with open(folder / "ToDoList.txt", "r+", encoding='utf-7') as file_to_do:
-            lines = file_to_do.readlines()
-            file_to_do.seek(0)
-            for i in range(len(lines)):
-                if lines[i].split("&")[1] == str(num):
-                    file_to_do.write(task.replace("\n", "") + "&" + str(num) + "&" + status + "\n")
-                else:
-                    file_to_do.write(lines[i])
-            file_to_do.truncate()
+    def create_todolist(self):
+        self.empty_lists()
+
+        randomizer = Randomizer()
+        lst = [
+            *randomizer.randomize_tasks_other_morning(),
+            *randomizer.randomize_tasks_other_afternoon(),
+            *randomizer.randomize_tasks_other_evening()
+        ]
+        for i, task in enumerate(lst):
+            self.todolist.append({'Task': task, 'ID': i + 1, 'Task Status': 'To-Do'})
+
+    def empty_lists(self):
+        self.todolist = []
+
+    def read_file(self):
+        with open(path, encoding='utf-8') as file_to_do:
+            if file_to_do.readline() == 'Task&ID&Task Status':
+                fieldnames = None
+            else:
+                fieldnames = ['Task', 'ID', 'Task Status']
+            csv_reader = csv.DictReader(file_to_do, fieldnames=fieldnames, delimiter='&')
+            self.todolist = list(csv_reader)
+
+    def change(self, identifier, status):
+        """Change status of task [task] with id [indentifier] from to-do list to status [status]."""
+        pos = next((i for i, item in enumerate(self.todolist) if int(item["ID"]) == identifier), None)
+        self.todolist[pos]['Task Status'] = status
+        self.write_to_file()
+
+    def remove_task(self, identifier):
+        pos = next((i for i, item in enumerate(self.todolist) if int(item["ID"]) == identifier), None)
+        self.todolist.pop(pos)
+        self.write_to_file()
+
+    def write_to_file(self):
+        with open(path, 'w', encoding='utf-8') as file_to_do:
+            csv_writer = csv.DictWriter(file_to_do, ['Task', 'ID', 'Task Status'], delimiter='&')
+            csv_writer.writeheader()
+            csv_writer.writerows(self.todolist)
 
 
-class CreateToDoList:
-    def __init__(self):
-        self.todo_list = self.list
+def main():
+    todolist = ToDoList()
+    print(todolist.todolist)
+    todolist.remove_task(0)
 
-    @staticmethod
-    def list(new: bool = True):
-        """Create to-do list from the randomizer if new is true and \n
-         and based on the status of all tasks when the new is false. """
 
-        tasks_list = []
-        if new:
-            tasks_list.extend(Randomizer().randomize_tasks_other_morning())
-            tasks_list.extend(Randomizer().randomize_tasks_other_afternoon())
-            tasks_list.extend(Randomizer().randomize_tasks_other_evening())
-            with open(folder / "ToDoList.txt", 'w', encoding='utf-7') as file_todo:
-                for i in range(len(tasks_list)):
-                    file_todo.writelines(tasks_list[i] + "&" + str(i) + "&" + "To-Do" + '\n')
-        else:
-            with open(folder / "ToDoList.txt", 'r+', encoding='utf-7') as file_todo:
-                lines = file_todo.readlines()
-                file_todo.seek(0)
-                i = 0
-                file_todo.truncate()
-                for line in lines:
-                    if "Removed" not in line and "Done" not in line:
-                        tasks_list.append(line.split("&")[0])
-                        file_todo.write(line.split("&")[0] + "&" + str(i) + "&" + line.split("&")[2])
-                        i += 1
-                    else:
-                        file_todo.write(line.split("&")[0] + "&" +
-                                        str(int(line.split("&")[1]) + 100) +
-                                        "&" + line.split("&")[2])
-        return tasks_list
+if __name__ == '__main__':
+    main()
