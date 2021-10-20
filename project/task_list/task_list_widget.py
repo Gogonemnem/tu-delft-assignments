60e57174f41393cc83ca2f5f23b5524bad0decfe
Nils <n.molenaar@student.tudelft.nl>
2021-10-20 12:37:28 +0200

    Add comments and remove inactive lines

--- project/task_list/task_list_widget.py
+++ project/task_list/task_list_widget.py
@@ -3,11 +3,14 @@ from project.task_list.to_do_list import ToDoList, CreateToDoList
 
 
 class TaskListWidget(QGroupBox):
+    """Visualise Task that need to be done today"""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setTitle("Daily to-do list")
         self.complete = 0
 
+        # Create groups for all button types
         self.group_task = QButtonGroup()
         self.group_remove = QButtonGroup()
         self.group_done = QButtonGroup()
@@ -17,6 +20,7 @@ class TaskListWidget(QGroupBox):
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
+        # Add all buttons for length of to do list
         for i, item in enumerate(self.tasks):
             self.task = self.create_task_select(i, item)
             self.remove = self.create_remove_button(i)
@@ -25,14 +29,17 @@ class TaskListWidget(QGroupBox):
 
             self.color_buttons(i)
 
-        # self.length_list = len(self.group_task.buttons())
-
     def removed(self, index):
         """Remove task from to-do list."""
+
         task_button = self.group_task.button(index)
         if task_button.isChecked():
+
+            # Change status of task in to-do list
             ToDoList.change(task_button.text().replace(
                 f'Task {index + 1} for today is: ', ''), self.group_task.id(self.group_task.checkedButton()), "Removed")
+
+            # clear task from widget
             self.group_task.button(index).setVisible(False)
             self.group_done.button(index).setVisible(False)
             self.group_remove.button(index).setVisible(False)
@@ -40,54 +47,45 @@ class TaskListWidget(QGroupBox):
 
     def ongoing(self, index):
         """Set status of task to "Doing"."""
+
         task_button = self.group_task.button(index)
         if task_button.isChecked():
+
+            # Change status of task in to-do list
             new_text = task_button.text().replace(f'Task {index + 1} for today is: ', '')
             ToDoList.change(new_text, index, "Doing")
+
+            # Change visual of task in widget
             self.group_remove.button(index).setVisible(False)
             self.group_done.button(index).setVisible(True)
 
     def completed(self, index):
         """Set status of task to "Done"."""
+
         task_button = self.group_task.button(index)
         if task_button.isChecked():
+
+            # Change status of task in to-do list
             old_text = f'Task {index + 1} for today is: '
             ToDoList.change(task_button.text().replace(old_text, ''), index, "Done")
 
+            # Change visual of task in widget
             selected_task = self.group_task.button(index)
             selected_doing = self.group_doing.button(index)
             selected_done = self.group_done.button(index)
-            # selected_remove = self.group_remove.button(index)
 
             selected_task.setDisabled(True)
             selected_doing.setVisible(False)
             selected_done.setVisible(False)
 
-            # ## Do we want this?????
-            # ## If so, selected_task may not be disabled!
-            # selected_remove.setVisible(True)
-
             selected_task.setStyleSheet("color:  rgb(100, 175, 100)")
             selected_task.setText(
                 selected_task.text().replace(old_text, '\u2713' + 'Completed: '))
-            # selected_done.setStyleSheet("background-color:  rgb(175, 175, 175)")
-
-            # ## Personal taste Question: Do we want to reorder the tasks?
-            # ## I actually like it staying in order like this
-            # ## This will also make our code a bit shorter
-            #
-            # self.group_task.removeButton(selected_task)
-            # self.group_doing.removeButton(selected_doing)
-            # self.group_done.removeButton(selected_done)
-            # self.group_remove.removeButton(selected_remove)
-            #
-            # self.layout.addWidget(selected_task, self.length_list + self.complete, 0)
-            # self.layout.addWidget(selected_doing, self.length_list + self.complete, 1)
-            # self.layout.addWidget(selected_done, self.length_list + self.complete, 2)
             self.complete += 1
 
     def create_task_select(self, index, item):
         """Visualize the selection radio button"""
+
         task = QRadioButton(f'Task {index + 1} for today is: {item}')
         task.setMinimumWidth(450)
 
@@ -100,6 +98,7 @@ class TaskListWidget(QGroupBox):
 
     def create_remove_button(self, index):
         """Visualize the selection remove button"""
+
         remove = QPushButton('Remove task')
         remove.setCheckable(True)
         remove.setMaximumWidth(100)
@@ -113,6 +112,7 @@ class TaskListWidget(QGroupBox):
 
     def create_doing_button(self, index):
         """Visualize the selection doing button"""
+
         doing = QPushButton('Do task')
         doing.setCheckable(True)
         doing.setMaximumWidth(100)
@@ -126,6 +126,7 @@ class TaskListWidget(QGroupBox):
 
     def create_done_button(self, index):
         """Visualize the selection done button"""
+
         done = QPushButton('Task completed')
         done.setCheckable(True)
         done.setVisible(False)
