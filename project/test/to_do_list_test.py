506d2017bb45bfe57ff8689470aa01c9bd8b86e1
Nils <n.molenaar@student.tudelft.nl>
2021-10-25 12:03:58 +0200

    Start with new to do list test

--- project/test/to_do_list_test.py
+++ project/test/to_do_list_test.py
@@ -2,47 +2,32 @@ import unittest
 from PyQt5.QtWidgets import QApplication
 import sys
 from project.randomizer.randomizer_of_tasks import Randomizer
-from project.task_list.task_list_widget import TaskListWidget
-
-
-class CallGroupBox:
-    def __init__(self):
-        app = QApplication(sys.argv)
-        self.window = TaskListWidget()
-        self.text = self.window.task.text()
-        self.buttons_task = self.window.group_task.buttons()
-        self.buttons_done = self.window.group_done.buttons()
-        self.buttons_remove = self.window.group_remove.buttons()
+from project.task_list.task_list_widget import ToDoList
 
 
 class TesToDoList(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.to_do_list = CallGroupBox().window
+        self.to_do_list = ToDoList().todolist
 
     def test_attribute(self):
-        self.assertTrue(hasattr(self.to_do_list, 'completed'))
-        self.assertTrue(hasattr(self.to_do_list, 'select'))
-        self.assertTrue(hasattr(self.to_do_list, 'removed'))
-        self.assertTrue(hasattr(self.to_do_list, 'ButtonGroup_task'))
-        self.assertTrue(hasattr(self.to_do_list, 'ButtonGroup_done'))
-        self.assertTrue(hasattr(self.to_do_list, 'ButtonGroup_remove'))
-        self.assertTrue(hasattr(self.to_do_list, 'complete'))
-        self.assertTrue(hasattr(self.to_do_list, 'tasks'))
-        self.assertTrue(hasattr(self.to_do_list, 'task'))
-        self.assertTrue(hasattr(self.to_do_list, 'done'))
-        self.assertTrue(hasattr(self.to_do_list, 'remove'))
-        self.assertTrue(hasattr(self.to_do_list, 'layout'))
+        self.assertTrue(hasattr(self.to_do_list, 'status'))
+        self.assertTrue(hasattr(self.to_do_list, 'todolist'))
+        self.assertTrue(hasattr(self.to_do_list, 'available'))
+        self.assertTrue(hasattr(self.to_do_list, 'create_todolist'))
+        self.assertTrue(hasattr(self.to_do_list, 'change'))
+        self.assertTrue(hasattr(self.to_do_list, 'read_file'))
+        self.assertTrue(hasattr(self.to_do_list, 'write_to_file'))
 
     def test_scenario(self):
-        rand_task = []
-        rand_task.extend(Randomizer().randomize_tasks_other_morning())
-        rand_task.extend(Randomizer().randomize_tasks_other_afternoon())
-        rand_task.extend(Randomizer().randomize_tasks_other_evening())
-        text = CallGroupBox().text.replace(f'Task {len(rand_task)} for today is: ', "")
-        self.assertEqual(rand_task[0][0], self.to_do_list.tasks[0])
-        self.assertEqual(rand_task[-1][-1], text)
-        self.assertTrue(len(rand_task) == len(self.to_do_list.tasks))
-        self.assertTrue(len(rand_task) == len(CallGroupBox().buttons_task))
-        self.assertTrue(len(rand_task) == len(CallGroupBox().buttons_done))
-        self.assertTrue(len(rand_task) == len(CallGroupBox().buttons_remove))
+        randomizer = Randomizer()
+        lst = [
+            *randomizer.randomize_tasks_other_morning(),
+            *randomizer.randomize_tasks_other_afternoon(),
+            *randomizer.randomize_tasks_other_evening()
+        ]
+        for task, status in enumerate(self.to_do_list):
+            self.assertEqual(self.to_do_list[task], lst[task])
+
+    def test_read_write(self):
+        print("test voor lezen schrijven maken")
