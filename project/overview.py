3b47f1fefab07e2df4c845366181a91e508e94ef
Gonem Lau <g.lau@student.tudelft.nl>
2021-12-16 12:36:59 +0100

    Add the tab for individual crypto/stocks

--- project/overview.py
+++ project/overview.py
@@ -1,9 +1,11 @@
 import pandas as pd
-from PyQt6.QtWidgets import QComboBox, QPushButton, QTabWidget, QTableView, QMainWindow, QHeaderView
+from PyQt6.QtWidgets import (QComboBox, QHeaderView, QMainWindow, QPushButton,
+                             QRadioButton, QTableView, QTabWidget)
 
 from project.dataframe_model import DataFrameModel
+from project.individual import Individual
 from project.input import Input
-
+from project.yfinancecrypto import apply_indicators, apply_signals
 
 column_names = ['Ticker', 'Name', 'Price', 'Advice']
 assets = [['AAPL', 'Apple Inc.', '100', 'hold'], ['AMZN', 'Amazon.com, Inc.', '100', 'hold']]
@@ -40,6 +42,9 @@ class Overview:
         header_crypto = self.view_crypto.horizontalHeader()
         header_crypto.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
+        results_button: QPushButton = self.main_window.findChild(QPushButton, "results_button")
+        results_button.clicked.connect(self.get_results)
+
     def set_dataframes(self):
         dataframe = self.input.model.dataFrame
 
@@ -48,13 +53,13 @@ class Overview:
 
         print(dataframe.loc[:, 'Symbol'].tolist())
 
-        first_box: QComboBox = self.main_window.findChild(QComboBox, "comboBox")
-        second_box = self.main_window.findChild(QComboBox, "comboBox_2")
+        first_select: QComboBox = self.main_window.findChild(QComboBox, "first_select")
+        second_select = self.main_window.findChild(QComboBox, "second_select")
 
-        first_box.clear()
-        second_box.clear()
-        first_box.addItems(dataframe.loc[:, 'Symbol'].tolist())
-        second_box.addItems(dataframe.loc[:, 'Symbol'].tolist())
+        first_select.clear()
+        second_select.clear()
+        first_select.addItems(dataframe.loc[:, 'Symbol'].tolist())
+        second_select.addItems(dataframe.loc[:, 'Symbol'].tolist())
 
     def calculate_advices(self):
         tab: QTabWidget = self.main_window.findChild(QTabWidget, "tabWidget")
@@ -64,6 +69,36 @@ class Overview:
 
         # ADD methods to add prices
         # ADD advices
+
+    def get_results(self):
+        single_button: QRadioButton = self.main_window.findChild(QRadioButton, "single_button")
+        compare_button: QRadioButton = self.main_window.findChild(QRadioButton, "compare_button")
+
+        if single_button.isChecked():
+            first_select: QComboBox = self.main_window.findChild(QComboBox, "first_select")
+            symbol = first_select.currentText()
+
+            if symbol:
+
+                # this is temporary, needs to be changed in overview-back
+                interval = '1d'
+                data = apply_indicators(symbol)
+                advice = apply_signals(data, symbol)
+                Individual(self.main_window, data, advice, symbol, interval)
+
+                tab: QTabWidget = self.main_window.findChild(QTabWidget, "tabWidget")
+                tab.setCurrentIndex(2)
+
+
+        elif compare_button.isChecked():
+            first_select: QComboBox = self.main_window.findChild(QComboBox, "first_select")
+            first_select.currentText()
+            second_select: QComboBox = self.main_window.findChild(QComboBox, "second_select")
+            second_select.currentText()
+
+            # finish this when comparison is done!
+
+
 
 # if __name__ == "__main__":
 #     app = QApplication(sys.argv)
